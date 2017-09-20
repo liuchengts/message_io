7533e1ee4d807410294ba4c01e52d5d2cc347dd4
刘成 <790809568@qq.com>
2017-09-20 17:01:02 +0800

    增加一个百度到的案例源码

---
+++ src/main/java/test/User.java
@@ -0,0 +1,30 @@
+package test;
+
+/**
+ * Created by apple on 2017/9/20.
+ */
+public class User {
+    private String name;
+    private String ip;
+
+    public User(String name, String ip) {
+        this.name = name;
+        this.ip = ip;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+}
